dfb88cb09538b9272c9694c7c82f462af7a1601f
Kiet Ho <hoakiet98@gmail.com>
2020-06-27 17:29:37 -0500

    adding materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 // Custom components
 import { AppComponent } from './app.component';
 
 // Custom modules
-import { AppRoutingModule } from './app-routing.module'
+import { AppRoutingModule } from './app-routing.module';
 import { MetronomeModule } from './metronome/metronome.module';
 
 
@@ -16,7 +18,12 @@ import { MetronomeModule } from './metronome/metronome.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MetronomeModule
+    MetronomeModule,
+    BrowserAnimationsModule,
+    MatSliderModule
+  ],
+  exports: [
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent],
