1f9a95da72b22009a33cf88a03a3989c4dd63d66
Kiet Ho <hoakiet98@gmail.com>
2020-06-28 19:40:15 -0500

    reconfigure for publish

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{practice-app}}'));
+app.use(express.static(__dirname + '/dist/practice-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/{{practice-app}}/index.html'));
+res.sendFile(path.join(__dirname+'/dist/practice-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
