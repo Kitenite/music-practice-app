9e8b6d339639b0115f71c2f09f800e7139bdc8f5
Kiet Ho <hoakiet98@gmail.com>
2020-07-02 17:52:12 -0500

    fixed sample rate to normal

--- src/app/tuner/components/tuner.component.ts
+++ src/app/tuner/components/tuner.component.ts
@@ -2,74 +2,60 @@ import { Component } from '@angular/core';
 import * as RecordRTC from 'recordrtc';
 import { DomSanitizer } from '@angular/platform-browser';
 
-
 @Component({
   selector: 'app-tuner',
   templateUrl: './tuner.component.html',
   styleUrls: ['./tuner.component.scss']
 })
 export class TunerComponent {
-  title = 'micRecorder';
-  //Lets declare Record OBJ
-  record;
-  //Will use this flag for toggeling recording
-  recording = false;
-  //URL of Blob
-  blobUrl;
-  error;
+
+  title:string = 'micRecorder';
+  record:any;
+  recording:boolean = false;
+  blobUrl:string;
+  error:any;
+
   constructor(private domSanitizer: DomSanitizer) {}
 
   sanitize(blobUrl: string) {
     return this.domSanitizer.bypassSecurityTrustUrl(blobUrl);
   }
-  /**
-  * Start recording.
-  */
+
   initiateRecording() {
-    this.recording = true;
     let mediaConstraints = {
-    video: false,
-    audio: true
+      video: false,
+      audio: true
     };
     navigator.mediaDevices.getUserMedia(mediaConstraints).then(this.successCallback.bind(this), this.errorCallback.bind(this));
   }
-  /**
-  * Will be called automatically.
-  */
+ 
   successCallback(stream) {
     var options = {
-    mimeType: "audio/wav",
-    numberOfAudioChannels: 1,
-    sampleRate: 16000,
+      mimeType: "audio/wav",
+      numberOfAudioChannels: 1,
+      sampleRate: 44100,
     };
     //Start Actuall Recording
     var StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
     this.record = new StereoAudioRecorder(stream, options);
     this.record.record();
+    this.recording = true;
+    console.log("recording")
   }
-  /**
-  * Stop recording.
-  */
+
   stopRecording() {
-  this.recording = false;
-  this.record.stop(this.processRecording.bind(this));
+    this.record.stop(this.processRecording.bind(this));
+    this.recording = false;
   }
-  /**
-  * processRecording Do what ever you want with blob
-  * @param  {any} blob Blog
-  */
+
   processRecording(blob) {
     this.blobUrl = URL.createObjectURL(blob);
-    console.log("blob", blob);
-    console.log("blobUrl", this.blobUrl);
+    console.log("blob: ", blob);
+    console.log("blobUrl : ", this.blobUrl);
   }
-  /**
-  * Process Error.
-  */
+
   errorCallback(error) {
     this.error = 'Can not play audio in your browser';
+    console.log(this.error)
   }
-  ngOnInit() {
-
-  }
-  }
+}
