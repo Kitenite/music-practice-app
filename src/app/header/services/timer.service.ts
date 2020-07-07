a9819500c60520413cca718f7b76d724f23cfcb9
Kiet Ho <hoakiet98@gmail.com>
2020-07-07 18:12:53 -0500

    fix timer delay

--- src/app/header/services/timer.service.ts
+++ src/app/header/services/timer.service.ts
@@ -9,24 +9,26 @@ export class TimerService {
 
   currentTime:number = 0;
   viewableTime:string = "00:00:00";
-  interval:any;
+  timeInterval:any = null;
   isPlaying:boolean = false;
+  
   toggleTimer() {
     if (!this.isPlaying){
       this.isPlaying = true;
-      this.interval = setInterval(() => {
+      this.timeInterval = setInterval(() => {
         this.currentTime+=1;
         this.viewableTime = new Date(this.currentTime * 1000).toISOString().substr(11, 8);
       },1000)
     } else{
-      clearInterval(this.interval);
+      clearInterval(this.timeInterval);
       this.isPlaying = false;
     }
   }
 
   resetTimer(){
     this.isPlaying = false;
-    clearInterval(this.interval);
+    clearInterval(this.timeInterval);
+    this.timeInterval = null;
     this.currentTime = 0;
     this.viewableTime = new Date(this.currentTime * 1000).toISOString().substr(11, 8);
   }
