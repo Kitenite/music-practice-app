6b4f747ac67c801074779416ed911d92fc579e4e
Kiet Ho <hoakiet98@gmail.com>
2020-07-11 00:58:43 -0500

    getting time

--- src/app/countdown-timer/components/countdown-timer.component.ts
+++ src/app/countdown-timer/components/countdown-timer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { count } from 'console';
 
 @Component({
   selector: 'app-countdown-timer',
@@ -9,7 +10,38 @@ export class CountdownTimerComponent implements OnInit {
 
   constructor() { }
 
+  timeOptions:TimeOption[] = [
+    {name:"hours", secondsMultiplier:3600, currentValue:0},
+    {name:"minutes", secondsMultiplier:60, currentValue:0},
+    {name:"seconds", secondsMultiplier:1, currentValue:0}
+  ]
+
+  timeUnit:number[]=[];
+  countdownTime:number;
+
   ngOnInit(): void {
+    this.generateUnits();
   }
 
+  generateUnits(){
+    for(var current:number = 0; current<60; current +=1){
+      this.timeUnit.push(current);
+    }
+  }
+
+  startTimer(){
+    this.countdownTime = 0;
+    this.timeOptions.forEach(option => {
+      this.countdownTime += option.currentValue*option.secondsMultiplier;
+    });
+    // TODO: Extract service for countdown here
+    console.log(this.countdownTime);
+  }
+
+}
+
+interface TimeOption{
+  name:string;
+  secondsMultiplier:number;
+  currentValue:number;
 }
