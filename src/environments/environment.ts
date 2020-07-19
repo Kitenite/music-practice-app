3f8202e1c08c478b8b7408c135185e833d8cf475
Kiet Ho <hoakiet98@gmail.com>
2020-07-19 12:09:42 -0500

    firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB6TaD3TQCgPdp3xMXPXpyKNpxRL4wbgF0",
+    authDomain: "practice-cd333.firebaseapp.com",
+    databaseURL: "https://practice-cd333.firebaseio.com",
+    projectId: "practice-cd333",
+    storageBucket: "practice-cd333.appspot.com",
+    messagingSenderId: "944597427836",
+    appId: "1:944597427836:web:bd49595e0efc2f43328bf9",
+    measurementId: "G-1N13NRX8W5"
+  }
 };
 
 /*
