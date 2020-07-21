af4b43e452d5e06d39d21597fa98b9ce5801e0c9
Kiet Ho <hoakiet98@gmail.com>
2020-07-20 23:49:52 -0500

    showing nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/practice', pathMatch: 'full' },
+  { path: '', redirectTo: 'practice', pathMatch: 'full' },
   { path: 'practice', loadChildren: () => import('./practice/practice.module').then(m => m.PracticeModule) }
 ];
 @NgModule({
--- src/app/practice/practice-routing.module.ts
+++ src/app/practice/practice-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PracticeComponent } from './components/practice.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'metronome', pathMatch: 'full' },
-  { path: 'metronome', loadChildren: () => import('./metronome/metronome.module').then(m => m.MetronomeModule) },
-  { path: 'tuner', loadChildren: () => import('./tuner/tuner.module').then(m => m.TunerModule) },
-  { path: 'drone', loadChildren: () => import('./drone/drone.module').then(m => m.DroneModule) },
-  { path: 'timer', loadChildren: () => import('./countdown-timer/countdown-timer.module').then(m => m.CountdownTimerModule) },
-  { path: 'record', loadChildren: () => import('./record/record.module').then(m => m.RecordModule) }
-];
+  { path: '', component: PracticeComponent,
+    children: [
+      { path: 'metronome', loadChildren: () => import('./metronome/metronome.module').then(m => m.MetronomeModule) },
+      { path: 'tuner', loadChildren: () => import('./tuner/tuner.module').then(m => m.TunerModule) },
+      { path: 'drone', loadChildren: () => import('./drone/drone.module').then(m => m.DroneModule) },
+      { path: 'timer', loadChildren: () => import('./countdown-timer/countdown-timer.module').then(m => m.CountdownTimerModule) },
+      { path: 'record', loadChildren: () => import('./record/record.module').then(m => m.RecordModule) }
+    ]}
+  ]
 
 @NgModule({
   imports: [
