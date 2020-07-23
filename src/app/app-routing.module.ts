dba040a8d981f06736a4e3267cae063bf9968877
Kiet Ho <hoakiet98@gmail.com>
2020-07-22 22:27:42 -0500

    create routing for main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'practice', pathMatch: 'full' },
-  { path: 'practice', loadChildren: () => import('./practice/practice.module').then(m => m.PracticeModule) }
+  { path: 'practice', loadChildren: () => import('./practice/practice.module').then(m => m.PracticeModule) },
+  { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) }
+
 ];
 @NgModule({
   imports: [
--- src/app/header/components/header.component.ts
+++ src/app/header/components/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  navLinks: any[];
+  activeLinkIndex = -1; 
 
+  constructor(private router: Router) {
+    this.navLinks = [
+      {
+        label: 'Practice',
+        link: '/practice',
+        icon: 'music_note',
+        disabled: false,
+        index: 0
+      }, {
+        label: 'Calendar',
+        link: '/calendar',
+        icon: 'calendar_today',
+        disabled: true,
+        index: 1
+      }, {
+        label: 'Account',
+        link: '/account',
+        icon: 'account_circle',
+        disabled: false,
+        index: 2
+      }
+    ];
+  }
   ngOnInit(): void {
+    this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => this.router.url.includes(tab.link)));
+      console.log
+  });
+  }
+
+  navButtonClicked(link){
+    this.router.navigate([link.link])
+    this.activeLinkIndex = link.index
   }
 
 }
