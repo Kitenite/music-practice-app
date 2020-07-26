76626b75fc6988bf7414397582a6ca89ba31968a
Kiet Ho <hoakiet98@gmail.com>
2020-07-26 16:17:08 -0500

    Firebase prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB6TaD3TQCgPdp3xMXPXpyKNpxRL4wbgF0",
+    authDomain: "practice-cd333.firebaseapp.com",
+    databaseURL: "https://practice-cd333.firebaseio.com",
+    projectId: "practice-cd333",
+    storageBucket: "practice-cd333.appspot.com",
+    messagingSenderId: "944597427836",
+    appId: "1:944597427836:web:bd49595e0efc2f43328bf9",
+    measurementId: "G-1N13NRX8W5"
+  }
 };
