67853215f5e0f11601293f29535a4ca2446aae3a
Kiet Ho <hoakiet98@gmail.com>
2020-08-16 13:31:21 -0500

    policy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'practice', pathMatch: 'full' },
   { path: 'practice', loadChildren: () => import('./practice/practice.module').then(m => m.PracticeModule) },
-  { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) }
+  { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
+  { path: 'privacy', loadChildren: () => import('./privacy/privacy.module').then(m => m.PrivacyModule) }
 
 ];
 @NgModule({
