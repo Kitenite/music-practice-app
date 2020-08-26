fcaafeb49868c71664d382ed54246fc845e17a28
Kiet Ho <hoakiet98@gmail.com>
2020-08-26 02:00:18 -0500

    coloring session events

--- src/app/shared/services/calendar.service.ts
+++ src/app/shared/services/calendar.service.ts
@@ -7,7 +7,9 @@ import { UserAuthService } from './user-auth.service';
 })
 export class CalendarService {
   calendarItems;
-  calendarSummary = 'Practice Calendar'
+  calendarSummary = 'Practice Sessions'
+  
+  eventColors = { blue:1,green:2,purple:3,red:4,yellow:5 }
 
   constructor(
     private afAuth:AngularFireAuth,
@@ -24,7 +26,7 @@ export class CalendarService {
       } else {
         const result = await this.userAuthService.gapi.client.calendar.calendars.insert({
           "summary": this.calendarSummary,
-          "description": "A calendar for logging practice sessions. Created by kpractice.\nColor-coding: Red: Less than 1 hour\nYellow: Between 1-2 hours\nBlue: Over Between 2-3 hours\nGreen: Over 4 hours"
+          "description": "A calendar for logging practice sessions.\nColor-coding:\n\tRed: Less than 1 hour\n\tYellow: Between 1-2 hours\n\tBlue: Over Between 2-3 hours\n\tGreen: Over 3 hours\n\nCreated by kpractice"
         })
         return result.result.id
       }
@@ -48,14 +50,55 @@ export class CalendarService {
             dateTime: endTime,
             timeZone: 'America/Los_Angeles'
           },
+          colorId: this.eventColors.red,
           summary: this.calendarSummary,
-          description: 'Red: Less than 1 hour\nYellow: Between 1-2 hours\nBlue: Over Between 2-3 hours\nGreen: Over 4 hours\nCreated by kpractice'
+          description: 'Color-coding:\n\tRed: Less than 1 hour\n\tYellow: Between 1-2 hours\n\tBlue: Over Between 2-3 hours\n\tGreen: Over 3 hours\n\nCreated by kpractice'
         })
+        this.updateSessionColors(calendarId)
       } else {
         alert("Something went wrong, try again")
         return 1
       }
     })
     return 0
+  }
+
+  async updateSessionColors(calendarId){
+    const date = (new Date()).toISOString().split('T')[0]
+    const timeMin = date+'T00:00:01Z'
+    const timeMax = date+'T23:59:59Z'
+    const eventList = await this.userAuthService.gapi.client.calendar.events.list({
+      calendarId: calendarId,
+      timeMin: timeMin,
+      timeMax: timeMax
+    })
+    var totalPracticeSeconds = 0
+    eventList.result.items.forEach(event => {
+      const startTime = event.start.dateTime.split('T')[1].split('Z')[0].split(':')
+      const endTime = event.end.dateTime.split('T')[1].split('Z')[0].split(':')
+      var startTimeSeconds = (+startTime[0]) * 60 * 60 + (+startTime[1]) * 60 + (+startTime[2]); 
+      var endTimeSeconds = (+endTime[0]) * 60 * 60 + (+endTime[1]) * 60 + (+endTime[2]); 
+      totalPracticeSeconds += (endTimeSeconds-startTimeSeconds) 
+    });
+    const totalPracticeHours = totalPracticeSeconds/3600
+    console.log("Total practice time today: ", totalPracticeHours)
+    var newEventColor;
+    if (totalPracticeHours < 1){
+      newEventColor = this.eventColors.red
+    } else if (totalPracticeHours < 2){
+      newEventColor = this.eventColors.yellow
+    } else if (totalPracticeHours < 3){
+      newEventColor = this.eventColors.blue
+    } else {
+      newEventColor = this.eventColors.green
+    }
+    eventList.result.items.forEach( async (event) => {
+      event.colorId = newEventColor
+      await this.userAuthService.gapi.client.calendar.events.update({
+        calendarId: calendarId,
+        eventId:event.id,
+        resource:event
+      })
+    })
   }
 }
